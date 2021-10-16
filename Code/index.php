cb3c5f1d3208f9aaca870e267df9dbf9a51b661f
unknown <rkhjfm@gmail.com>
2021-10-16 10:07:23 +0330

    file added

---
+++ Code/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Search the web for sites and images">
+    <meta name="keywords" content="Search engine, Ocean, websites">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <title>Ocean</title>
+</head>
+<body>
+
+    <div class="wrapper indexpage">
+        <div class="mainSection">
+        
+            <div class="logoContainer">
+                <img src="assets/images/oceanlogo-removebg.png" title="Logo of our Site" alt="Site Logo" >
+            </div>
+        
+            <div class="searchContainer">
+
+
+                <form action="search.php" method="GET">
+
+                    <input class="searchBox" type="text" name="term">
+                    <input class="searchButton" type="submit" value="search"> 
+        
+                </form>
+
+
+            </div>
+
+        </div>
+    </div>
+
+</body>
+</html>
