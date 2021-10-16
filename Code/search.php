b09e1ce6f4dd216d02da1b307b3a0b4210f5d0bb
unknown <rkhjfm@gmail.com>
2021-10-16 10:09:42 +0330

    file added

---
+++ Code/search.php
@@ -0,0 +1,166 @@
+<?php
+include("config.php");
+include("classes/SiteResultsProvider.php");
+include("classes/ImageResultsProvider.php");
+
+if(isset($_GET["term"])) {
+	$term = $_GET["term"];
+}
+else {
+	exit("You must enter a search term");
+}
+
+$type = isset($_GET["type"]) ? $_GET["type"] : "sites";
+$page = isset($_GET["page"]) ? $_GET["page"] : 1;
+
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+    <title>Ocean</title>
+</head>
+<body>
+
+    <div class="wrapper">
+         
+        <div class="header">
+
+            <div class="headerContent">
+
+                <div class="logoContainer">
+                  <a href="index.php">
+                    <img src="assets/images/oceanlogo-removebg.png" alt="">
+                  </a>
+                </div>
+                
+                <div class="searchContainer">
+                    <form action="search.php" method="GET">
+
+                    <div class="searchBarContainer">
+                        <input type="hidden" name="type" value="<?php echo $type; ?>">
+                        <input type="text" class="searchBox" name="term" value="<?php echo $term; ?>">
+                        <button class="searchButton">
+                            <img src="assets/images/icons/search-removebg.png" alt="">
+                        </button>
+                    </div>
+
+                    </form>
+                </div><!--searchContainer-->
+
+            </div><!--headerContent-->
+
+            <div class="tabsContainer">
+                <ul class="tabList">
+                    <li class="<?php echo $type == 'sites' ? 'active' : '' ?>">
+                        <a href='<?php echo "search.php?term=$term&type=sites";?>'>
+                        Sites    
+                        </a>              
+                    </li>
+
+                    <li class="<?php echo $type == 'images' ? 'active' : '' ?>">
+                        <a href='<?php echo "search.php?term=$term&type=images";?>'>
+                        Images
+                        </a>
+                    </li>
+                </ul>
+            </div><!--tabsContainer-->
+
+        </div><!--header-->
+
+
+        <div class="mainResultsSection">
+
+        <?php
+			if($type == "sites") {
+				$resultsProvider = new SiteResultsProvider($con);
+				$pageSize = 20;
+			}
+			else {
+				$resultsProvider = new ImageResultsProvider($con);
+				$pageSize = 30;
+			}
+
+			$numResults = $resultsProvider->getNumResults($term);
+
+			echo "<p class='resultsCount'>$numResults results found</p>";
+
+
+
+			echo $resultsProvider->getResultsHtml($page, $pageSize, $term);
+			?>
+
+        </div><!--mainresultssection-->
+
+
+        <div class="paginationContainer">
+
+             <div class="pageButtons">
+
+                <div class="pageNumberContainer">
+                     <img src="assets/images/pageStart.png">
+                 </div><!--pageNumberContainer-->
+
+                 <?php
+                 
+ 				$pagesToShow = 10;
+				$numPages = ceil($numResults / $pageSize);
+				$pagesLeft = min($pagesToShow, $numPages);
+
+				$currentPage = $page - floor($pagesToShow / 2);
+
+				if($currentPage < 1) {
+					$currentPage = 1;
+				}
+
+				if($currentPage + $pagesLeft > $numPages + 1) {
+					$currentPage = $numPages + 1 - $pagesLeft;
+				}
+
+				while($pagesLeft != 0 && $currentPage <= $numPages) {
+
+					if($currentPage == $page) {
+						echo "<div class='pageNumberContainer'>
+								<img src='assets/images/pageSelected.png'>
+								<span class='pageNumber'>$currentPage</span>
+							</div>";
+					}
+					else {
+						echo "<div class='pageNumberContainer'>
+								<a href='search.php?term=$term&type=$type&page=$currentPage'>
+									<img src='assets/images/page.png'>
+									<span class='pageNumber'>$currentPage</span>
+								</a>
+						</div>";
+					}
+
+
+					$currentPage++;
+					$pagesLeft--;
+
+				}
+
+
+                ?>
+
+                <div class="pageNumberContainer">
+                   <img src="assets/images/pageEnd.png">
+                </div><!--pageNumberContainer-->
+
+            </div><!--pageButtons-->
+
+        </div><!--paginationContainer-->
+        
+    </div><!--wrapper-->
+
+    <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+    <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
